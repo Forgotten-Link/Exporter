4f9b531751e98cc8204b8653269949ace9b5b065
Alexander <33471981+Forgotten-Link@users.noreply.github.com>
2022-03-10 15:47:51 -0800

    updated the main py

--- TroubleshootDataExport.py
+++ TroubleshootDataExport.py
@@ -14,26 +14,31 @@ def write():
     outfile = open(values['-file-'], 'a')
     if os.stat(values['-file-']).st_size == 0:
         outfile.write("Name" +';')
-        outfile.write("Chamber" +';')
+        outfile.write("Station" +';')
         outfile.write("Slot" +';')
         outfile.write("Fail Code" +';')
         outfile.write("Fix Description" +';')
         outfile.write("Date" + "\n")
         
     outfile.write(values['-Name-']+';')
-    outfile.write(values['-Chamber-']+';')
+    outfile.write(values['-Station-']+';')
     outfile.write(values['-Slot-']+';')
     outfile.write(values['-FailCode-']+';')
     outfile.write(values['-FixDescription-']+';')
     outfile.write(date + "\n")
     window['-Name-']('')
-    window['-Chamber-']('')
+    window['-Station-']('')
     window['-Slot-']('')
     window['-FailCode-']('')
     window['-FixDescription-']('')
     sg.Popup('Done',title = 'Success',auto_close = True,auto_close_duration = 0.5)
+    window['-Name-'].SetFocus(force = True)
+    window.refresh
+    
+    
         
     outfile.close()
+
 def convert():
     p = Path(values['-file-'])
     p.rename(p.with_suffix(fext))
@@ -42,38 +47,45 @@ def convert():
 
 
 layout = [
-    [sg.Text('Select File'),sg.In(readonly=True,key='-file-'), sg.FileBrowse()],
+    [sg.Text('Select File'),sg.In(readonly=True,key='-file-'), sg.FileBrowse(key='-Browse-'),],
 
     [sg.Text('')],
     [sg.Text('Name', size=(15, 1)),sg.InputText(key='-Name-')],
-    [sg.Text('Chamber', size=(15, 1)),sg.InputText(key='-Chamber-')],
+    [sg.Text('Station', size=(15, 1)),sg.InputText(key='-Station-')],
     [sg.Text('Slot', size=(15, 1)),sg.InputText(key='-Slot-')],
     [sg.Text('Fail Code', size=(15, 1)),sg.InputText(key='-FailCode-')],
     [sg.Text('Fix Description', size=(15, 1)),sg.InputText(key='-FixDescription-')],
-    [sg.Button('Submit',bind_return_key=True)],
+    [sg.Button('Submit',bind_return_key=True,key='-Submit-')],
 
     [sg.Text('_______________')],
     [sg.Text('File Options')],
     [sg.Radio('txt', "RADIO1", default=True,key='-txt-')],
     [sg.Radio('csv', "RADIO1",key='-csv-')],
-    [sg.Button('Convert')]
+    [sg.Button('Convert',key='-Convert-')]
 
 ]
 
-window = sg.Window('Troubleshoot Data Export', layout, size=(500,400))
+window = sg.Window('Troubleshoot Data Export', layout, size=(500,400),use_default_focus=False,finalize=True)
+
+window['-file-'].block_focus(block = True)
+window['-Browse-'].block_focus(block = True)
+window['-Submit-'].block_focus(block = True)
+window['-txt-'].block_focus(block = True)
+window['-csv-'].block_focus(block = True)
+window['-Convert-'].block_focus(block = True)
 
 try:
     while True:
       event, values = window.read()
       if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
            break
-      if event == 'Submit':
+      if event == '-Submit-':
            write()
       if values['-txt-'] == True:
            fext = '.txt'
       if values['-csv-'] == True:
            fext = '.csv'
-      if event == 'Convert':
+      if event == '-Convert-':
            convert()
 except Exception as e:
     tb = traceback.format_exc()
